0feb3e1895f111562e77439e9c6746e6bf1409d3
Rikk <tarikmoussa90@gmial.com>
2026-04-13 23:30:16 -0300

    feat: implementação dos formatos, regras de negócio e orientação do CP.

--- src/br/com/fiap/main/MainAluno.java
+++ src/br/com/fiap/main/MainAluno.java
@@ -1,13 +1,30 @@
 package br.com.fiap.main;
 
+import br.com.fiap.bean.Aluno;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+//CP2 de JAVA - 14/04/2026 - Pro.Gilberto
+//Tárik Moussa Alma - RM: 571411
+//Giovane Dias Chicoli - RM: 569750
+//Fabricio Aquiles Sales da Silva - RM: 570985
 public class MainAluno {
     public static void main(String[] args) {
 
         //Formato de data padrão dos EUA e pedido nos slides
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate dataAtual = LocalDate.now();
+
+        //Tópico 2 do CP - Dois construtores vazios da classe aluno
+        Aluno aluno1 = new Aluno();
+        aluno1.setRegistroMatricula(571411);
+        aluno1.setNomeCompleto("Tárik Moussa Alma");
+        aluno1.setDataDeNascimento(LocalDate.parse("03/02/2008", dtf));
+
+        Aluno aluno2 = new Aluno();
+        aluno2.setRegistroMatricula(570985);
+        aluno2.setNomeCompleto("Fábricio Aquiles Sales da Silva");
+        aluno2.setDataDeNascimento(LocalDate.parse("digite", dtf));
     }
 }
